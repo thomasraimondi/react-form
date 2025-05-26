689aee97d788387c48d218ec004c440aa69fd9c5
thomasraimondi <thomas.raimondi14@gmail.com>
2025-05-26 21:03:34 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import products from "./data/products";
+
+function App() {
+  return (
+    <div className="container">
+      <div className="header d-flex justify-content-between p-3">
+        <div className="title">
+          <h1>Form</h1>
+        </div>
+        <div className="function">
+          <button className="btn btn-primary">+</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/data/products.js
@@ -0,0 +1,47 @@
+export default [
+  {
+    id: 1,
+    title: "Product 1",
+    description: "lorem lorem lorem lorem lorem lorem lorem",
+  },
+  {
+    id: 2,
+    title: "Product 2",
+    description: "lorem lorem lorem lorem lorem lorem lorem",
+  },
+  {
+    id: 3,
+    title: "Product 3",
+    description: "lorem lorem lorem lorem lorem lorem lorem",
+  },
+  {
+    id: 4,
+    title: "Product 4",
+    description: "lorem lorem lorem lorem lorem lorem lorem",
+  },
+  {
+    id: 5,
+    title: "Product 5",
+    description: "lorem lorem lorem lorem lorem lorem lorem",
+  },
+  {
+    id: 6,
+    title: "Product 6",
+    description: "lorem lorem lorem lorem lorem lorem lorem",
+  },
+  {
+    id: 7,
+    title: "Product 7",
+    description: "lorem lorem lorem lorem lorem lorem lorem",
+  },
+  {
+    id: 8,
+    title: "Product 8",
+    description: "lorem lorem lorem lorem lorem lorem lorem",
+  },
+  {
+    id: 9,
+    title: "Product 9",
+    description: "lorem lorem lorem lorem lorem lorem lorem",
+  },
+];
