c7bd6cff9e3aecb090da92518c394addf4798fba
thomasraimondi <thomas.raimondi14@gmail.com>
2025-05-26 21:10:59 +0200

    add product card

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,20 @@ function App() {
           <button className="btn btn-primary">+</button>
         </div>
       </div>
+      <div className="main">
+        <div className="row row-gap-3">
+          {products.map((product) => (
+            <div className="col-4">
+              <div className="card">
+                <div className="card-header">
+                  #{product.id} - {product.title}
+                </div>
+                <div className="card-body">{product.description}</div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
