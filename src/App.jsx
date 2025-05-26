1dd328354c1f9f8d57090103f8cd5f4f976093ba
thomasraimondi <thomas.raimondi14@gmail.com>
2025-05-26 21:34:31 +0200

    add form for add new item

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
+import { useState } from "react";
 import products from "./data/products";
 
 function App() {
+  const [isFormVisible, setIsFormVisible] = useState(false);
+  const [submitForm, setSubmitForm] = useState();
+
+  const handleFormVisible = () => setIsFormVisible(!isFormVisible);
+  const handleSubmitform = (e) => {
+    e.preventDefault();
+    console.log("form inviato");
+  };
+
   return (
     <div className="container">
       <div className="header d-flex justify-content-between p-3">
@@ -8,13 +18,39 @@ function App() {
           <h1>Form</h1>
         </div>
         <div className="function">
-          <button className="btn btn-primary">+</button>
+          <button className="btn btn-primary" onClick={handleFormVisible}>
+            add
+          </button>
         </div>
       </div>
       <div className="main">
+        {isFormVisible ? (
+          <div className="col-4">
+            <form className="form-control my-4 d-flex flex-column gap-3">
+              <div className="field-1">
+                <label className="col-3  me-3" htmlFor="title">
+                  Titolo:
+                </label>
+                <input className="grow-1" type="text" name="title" id="title" />
+              </div>
+              <div className="field-2">
+                <label className="col-3 me-3" htmlFor="description">
+                  Descrizione
+                </label>
+                <input type="text" name="description" id="description" />
+              </div>
+              <button onClick={handleSubmitform} className="btn btn-primary">
+                Add
+              </button>
+            </form>
+          </div>
+        ) : (
+          ""
+        )}
+
         <div className="row row-gap-3">
           {products.map((product) => (
-            <div className="col-4">
+            <div className="col-4" key={product.id}>
               <div className="card">
                 <div className="card-header">
                   #{product.id} - {product.title}
